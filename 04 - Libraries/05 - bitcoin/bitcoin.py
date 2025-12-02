172447225005e255bb702831bc77aa2b2b09df94
Lucas <72714237+lucfercosta@users.noreply.github.com>
2025-12-02 08:14:07 +0000

    bitcoin.py

--- 04 - Libraries/05 - bitcoin/bitcoin.py
+++ 04 - Libraries/05 - bitcoin/bitcoin.py
@@ -1,11 +1,9 @@
 import sys
 import requests
 
-bitcoin_price = 0
-bitcoin_total = 0
+
 try:
-    user_price_request = sys.argv[1]
-    user_price_request = float(user_price_request)
+    user_price_request = float(sys.argv[1])
 except IndexError:
     print("Missing command-line argument")
     sys.exit()
@@ -14,11 +12,13 @@ except ValueError:
     sys.exit()
         
 
+# Get request
 api_request = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=8b17fb4826cfc5f07c45a0c3265dd4e16d74abfca19848e0e4dd7bfb6e2d761b")
-
-for key, value in api_request.items():
-    if key == "priceUsd":
-        bitcoin_price = value
-
+# Convert request into json
+data = api_request.json()
+# Get priceUsd from json
+bitcoin_price = float(data["data"]["priceUsd"])
+# Get total price
 bitcoin_total = bitcoin_price * user_price_request
+# Print
 print(f"{bitcoin_total:.4f}")
